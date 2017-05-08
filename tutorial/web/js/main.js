ec5dfae7ecb8d6ab9d906ea6ec8f84b7bf316f39
Ryan Weaver <ryan@thatsquality.com>
2017-05-08 13:05:57 -0400

    removing old tutorial directory

--- tutorial/web/js/main.js
+++
@@ -1,9 +0,0 @@
-+function ($) {
-
-    $(document).ready(function() {
-        $('.js-header-search-toggle').on('click', function() {
-            $('.search-bar').slideToggle();
-        });
-    });
-
-}(jQuery);
